b345d1189d7fd867799f7589b064a26bbbb967d3
GabrielPruculi <106412047+GabrielPruculi@users.noreply.github.com>
2022-05-27 22:06:18 -0300

    Add files via upload

---
+++ aula-gabrielp-2022-05-27.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+ printf("Oi, mundo\n");
+ printf("%10d \n", 13);
+ printf("%05.2f  \n",  7.14);
+ printf("%05.2f \n",  27.20);
+ printf("%30s \n", "gabriel");
+ printf("nome: %s idade:%d \n", "Gabriel", 22);
+
+
+}
+
