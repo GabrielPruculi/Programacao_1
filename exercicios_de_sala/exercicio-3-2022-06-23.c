6d8b21bb8e6ef684caaa3408c81bea79a39f8eff
GabrielPruculi <106412047+GabrielPruculi@users.noreply.github.com>
2022-06-23 19:02:34 -0300

    Create exercicio-3-2022-06-23.c

---
+++ exercicios_de_sala/exercicio-3-2022-06-23.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main ()
+{
+int div;
+printf ("Os divisores de 3 são: \n");
+div = 1;
+while (div<=40)
+{
+if (div % 3 == 0)
+printf ("Os divisores de 3 são: %d \n", div);
+div++;
+
+}
+
+
+
+
+
+
+
+}
