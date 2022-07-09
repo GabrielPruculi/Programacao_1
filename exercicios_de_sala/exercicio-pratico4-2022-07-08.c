c75da2866c722c5758e36116d34a5980070140b1
GabrielPruculi <106412047+GabrielPruculi@users.noreply.github.com>
2022-07-08 21:48:19 -0300

    Create exercicio-pratico4-2022-07-08.c

---
+++ exercicios_de_sala/exercicio-pratico4-2022-07-08.c
@@ -0,0 +1,19 @@
+//Crie um vetor de 15 posições só com números ímpares.
+#include <stdio.h>
+int main () {
+int vet[100], i;
+for (i=0; i<100; i++){
+vet[i]= 2*i+1;
+}
+for (i=0; i<100; i++) {
+printf ("%d \n", vet[i]);
+
+}
+
+
+
+
+
+
+
+}
