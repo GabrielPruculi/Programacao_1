652d60399a991a8f5fb5de266d3991a4573ca4ec
GabrielPruculi <106412047+GabrielPruculi@users.noreply.github.com>
2022-08-04 19:45:04 -0300

    Update ponteiros-com-vetor-2022-08-04.c

--- exercicios_de_sala/ponteiros-com-vetor-2022-08-04.c
+++ exercicios_de_sala/ponteiros-com-vetor-2022-08-04.c
@@ -15,3 +15,49 @@ int main () {
 	printf("media: %d\n", soma/3);
 
 }
+
+-----------------------------------------------------------------------------------------------------------------------------------------------
+	#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+	int *vetor, i, *soma;
+	vetor = malloc(3*sizeof(int));
+	soma = malloc(sizeof(int));
+	*soma=0;
+	for (i=0;i<3; i++){
+		printf("Digite valor:\n");
+		scanf("%d", &vetor[i]);
+	}
+	
+	for (i=0;i<3; i++){
+		*soma = *soma +vetor[i];
+	}
+	printf("media: %d\n", *soma/3);
+
+}
+
+-----------------------------------------------------------------------------------------------------------------------------------------------
+	
+	#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+	int *vetor, *i, *soma;
+	vetor = malloc(3*sizeof(int));
+	i=malloc(sizeof(int));
+	soma = malloc(sizeof(int));
+	*soma=0;
+	for (*i=0;*i<3; (*i)++){
+		printf("Digite valor:\n");
+		scanf("%d", &vetor[*i]);
+	}
+	
+	for (*i=0;*i<3; (*i)++){
+		*soma = *soma +vetor[*i];
+	}
+	printf("media: %d\n", *soma/3);
+
+
+
+}
