371a0e6fc7caf38b11054b6bf13bb3cb999da22d
GabrielPruculi <106412047+GabrielPruculi@users.noreply.github.com>
2022-08-11 19:49:12 -0300

    Update recursividade-2022-08-11.c

--- exercicios_de_sala/recursividade-2022-08-11.c
+++ exercicios_de_sala/recursividade-2022-08-11.c
@@ -13,3 +13,29 @@ int main () {
 
 
 }
+----------------------------------------------------------------------------------------------------------------------------------------------------
+	
+	#include <stdio.h>
+#include <stdlib.h>
+	
+int p_impar(int vet[5], int pos){
+	if (pos>=0 && pos<5)
+	{
+		if (vet[pos]%2 == 1){
+			return 1; 
+	}else{ 
+		return p_impar(vet, pos+1);
+		}
+	}else{
+			return 0;
+		}
+
+}
+int main () {
+	
+	int v[5] = {2,4,6,1,7}, resp;
+	resp=p_impar(v,0);
+	printf("Resp: %s\n", (resp==1)?"sim":"não");
+
+
+}
